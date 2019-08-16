5cf65b0cfdb81d593681daee39d4df7f62f0dd4d
Lucas Nathan Barbosa de Oliveira <lucas.nathan80@hotmail.com>
2019-08-16 13:10:12 -0300

    operacao adicao

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from operacoes import basicas
+
 def main():
-  return 0
+  print('1# operando')
+  a = input()
+  print('2# operando')
+  b = input()
+  print(basicas.adicao(a,b))
 
 if __name__ == "__main__":
   main()
