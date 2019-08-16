91799c1fd331143e3d82a7d5ec3ab341cc710163
Lucas Nathan Barbosa de Oliveira <lucas.nathan80@hotmail.com>
2019-08-16 13:21:43 -0300

    estrutura de operacoes

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 def main():
-  return 0
+  print('por favor digite o número correspondente a sua operação:')
+  print('1 - adicao')
+  print('2 - subtracao')
+  print('3 - divisao')
+  print('4 - multiplicacao')
+  operacao = int(input())
+  if operacao == 1:
+    print('adicao')
+  elif operacao == 2:
+    print('subtracao')
+  elif operacao == 3:
+    print('divisao')
+  elif operacao == 4:
+    print('multiplicacao')
+  else:
+    print('operacao não implementada')
 
 if __name__ == "__main__":
   main()
